4ed8025a8494ff90a158dd1ec64f2e2738dbaff6
streetlightkids <ltj_hq@hotmail.com>
2020-07-27 11:49:09 -0700

    added bootstrap in a new and improved way

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Whether Weather
-      <button className="btn btn-primary"> Butts </button>
-      <footer>
-        Open source code by
-        <a
-          href="https://github.com/streetlightkids/react-final-project"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Nikki Klein
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Whether Weather</h1>
+        <br />
+        <button className="btn btn-primary"> Butts </button>
+        <footer>
+          Open source code by
+          <a
+            href="https://github.com/streetlightkids/react-final-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Nikki Klein
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
