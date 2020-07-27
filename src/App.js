c9334906f7677978d76e50d1b08aa01b01d088de
streetlightkids <ltj_hq@hotmail.com>
2020-07-27 11:29:02 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Whether Weather</div>;
+  return (
+    <div className="App">
+      Whether Weather
+      <footer>
+        Open source code by
+        <a
+          href="https://github.com/streetlightkids/react-final-project"
+          target="_blank"
+        >
+          {" "}
+          Nikki Klein
+        </a>
+      </footer>
+    </div>
+  );
 }
