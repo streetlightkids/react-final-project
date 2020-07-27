af34f13e9938bff662730539d5cbc075559dbe67
streetlightkids <ltj_hq@hotmail.com>
2020-07-27 11:38:12 -0700

    testing bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ export default function App() {
   return (
     <div className="App">
       Whether Weather
+      <button className="btn btn-primary"> Butts </button>
       <footer>
         Open source code by
         <a
           href="https://github.com/streetlightkids/react-final-project"
           target="_blank"
+          rel="noopener noreferrer"
         >
           {" "}
           Nikki Klein
