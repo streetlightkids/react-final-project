b0d35009a13d251d9a844a9127f28b4348978e60
streetlightkids <ltj_hq@hotmail.com>
2020-07-28 12:55:15 -0700

    Created codemap const
    set up new animated icons

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
       city: response.data.name,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
     });
   }
